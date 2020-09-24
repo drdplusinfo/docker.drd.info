82904f0ea639c09f0fd2df324d1f08550212d08b
Jaroslav Týc <mail@jaroslavtyc.com>
2020-09-24 16:53:17 +0200

    Caddy configs split

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -7,7 +7,7 @@ nohup php-fpm &
 echo PROJECT_ENVIRONMENT=$PROJECT_ENVIRONMENT
 
 if [[ $(grep --invert-match --ignore-case ^dev <<< "$PROJECT_ENVIRONMENT") ]]; then
-  /usr/bin/caddy run --config /etc/caddy/production/Caddyfile --watch
+  /usr/bin/caddy run --config /etc/caddy/production/Caddyfile
 else
   /usr/bin/caddy run --config /etc/caddy/localhost/Caddyfile --watch
 fi
