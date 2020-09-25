f12fadc9d8c3c106a8bb423e0eaebfe19e3f0576
Jaroslav Týc <mail@jaroslavtyc.com>
2020-09-25 10:18:19 +0200

    Env variable with project environment value is required

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -6,7 +6,12 @@ nohup php-fpm &
 
 echo PROJECT_ENVIRONMENT=$PROJECT_ENVIRONMENT
 
-if [[ $(grep --invert-match --ignore-case ^dev <<< "$PROJECT_ENVIRONMENT") ]]; then
+if [[ -z "$PROJECT_ENVIRONMENT" ]]; then
+  echo "Missing PROJECT_ENVIRONMENT environment variable"
+  exit 1
+fi
+
+if [[ $(grep --invert-match --ignore-case ^dev <<<"$PROJECT_ENVIRONMENT") ]]; then
   /usr/bin/caddy run --config /etc/caddy/production/Caddyfile
 else
   /usr/bin/caddy run --config /etc/caddy/localhost/Caddyfile --watch
